f1298c5951f93d15db09b88b91f03281285e5bd9
Parq254 <juniorchris761@gmail.com>
2024-07-05 11:48:44 +0300

    class objects

--- class_practice.py
+++ class_practice.py
@@ -1,11 +1,20 @@
-class Number:
-    def __init__(self):
-        self.var = 24 + 24
+class person(object):
+    population = 50
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def getpopulation(cls):
+        return cls.population
+
+    @staticmethod
+    def isadult(age):
+        return age >= 18
 
     def display(self):
-        print(self.var)
+        print(self.name, 'is', self.age, 'years old')
 
-newNumber = Number()
-newNumber.display()
-#Update
-#set
+newperson = person('Chris', 20)
+
+print(person.getpopulation())
