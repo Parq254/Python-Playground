08e1a3cb362b166a64519c28856a083bbce77462
Parq254 <juniorchris761@gmail.com>
2024-07-05 11:27:49 +0300

    update on optional

--- optional_parameters.py
+++ optional_parameters.py
@@ -1,17 +1,18 @@
-class Car(object):
-    def __init__(self, make, model, year, condition='New', kms=0):
+class car(object):
+    def __init__(self, make, model, year, condition, kms):
         self.make = make
         self.model = model
         self.year = year
         self.condition = condition
         self.kms = kms
 
-    def display(self, showAll= True):
-        if showAll:
-            print('This car is a %s %s from %s, it is %s and has %s kms.'%(self.make, self.model,
-                                                                            self.year, self.condition, self.kms))
+    def display(self, showall):
+        if showall:
+            print('This car is a %s %s from %s,it is %s and has %s kms.'
+                  % (self.make, self.model, self.year, self.condition, self.kms))
         else:
-            print('This car is a %s is from %s'%(self.make, self.model, self.year))
+            print('This car is %s %s from %s' % (self.make, self.model, self.year))
 
-whip = Car ('Ford', 'Fussion', 2012)
+whip = car('Ford', 'Fusion', 2012, 'New', 0)
 whip.display(True)
+
