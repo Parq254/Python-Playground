fea92ebe43632ed8684a4d0d1272a93f85fa94c5
Parq254 <juniorchris761@gmail.com>
2024-07-07 13:59:37 +0300

    counter update

--- collections/counter().py
+++ collections/counter().py
@@ -11,7 +11,13 @@ c = Counter(cats=4, dogs=7)
 print(c)
 '''
 
+'''
 c = Counter(a=4, b=2, c=0, d=-2)
 d = ['a', 'b', 'b', 'c']
 c.subtract(d)
 print(c)
+'''
+
+c = Counter([1, 1, 1, 3, 4, 5, 6, 7, 7])
+feedback = c.most_common(1)
+print(feedback)
